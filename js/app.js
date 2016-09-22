9eb4a678bd53566b6ca9222cf984da87cd2334f1
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-09-22 16:31:45 -0400

    starting controller

---
+++ js/app.js
@@ -0,0 +1,12 @@
+angular.module('myApp', [])
+	.controller('myCtrl', function($scope){
+		$scope.femaleName = "";
+		$scope.jobTitle = "";
+		$scope.tediousTask = "";
+		$scope.dirtyTask = "";
+		$scope.celebrity = "";
+		$scope.uselessSkill = "";
+		$scope.adjective = "";
+		$scope.obnoxiousCelebrity = "";
+		$scope.hugePercent = "";
+	});
