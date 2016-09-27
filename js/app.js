93321b7045b7d334544fa5f95302e8061b0f3ceb
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-09-27 09:36:33 -0400

    gender pronouns work again, reset button works

--- js/app.js
+++ js/app.js
@@ -1,31 +1,49 @@
 angular.module('myApp', [])
-	.controller('myCtrl', function(){
-		var main = this;
-		main.submitMadLib = function(){
-			main.myForm.$submitted=true;
-		};
-		main.startingPoint = function(){
-		main.name = "";
-		main.jobTitle = "";
-		main.tediousTask = "";
-		main.dirtyTask = "";
-		main.celebrity = "";
-		main.uselessSkill = "";
-		main.adjective = "";
-		main.obnoxiousCelebrity = "";
-		main.hugeNumber = "";
-		main.genderObject = "she";
-		main.genderPronoun = "her";
-		main.genderSpecial = "her";
-		main.genderificationMale = function(){
-			main.genderObject = "he";
-			main.genderPronoun = "his";
-			main.genderSpecial = "him";
-		};
-		main.genderificationFemale = function(){
-			main.genderObject = "she";
-			main.genderPronoun = "her";
-			main.genderSpecial = "her";
-		};
-		};
-	});
+    .controller('myCtrl', function() {
+        var main = this;
+        main.submitted = false;
+        main.gender = "";
+
+        main.submitMadLib = function(valid) {
+            if (valid) {
+                main.submitted = true;
+
+                if (main.gender == "male") {
+                    genderificationMale();
+                } else {
+                    genderificationFemale();
+                }
+
+            } else {
+                alert('The number is not valid, please enter a positive number');
+            }
+
+        };
+
+        var genderificationMale = function() {
+            main.genderObject = "he";
+            main.genderPronoun = "his";
+            main.genderSpecial = "him";
+        };
+
+        var genderificationFemale = function() {
+            main.genderObject = "she";
+            main.genderPronoun = "her";
+            main.genderSpecial = "her";
+        };
+        main.startingPoint = function() {
+            main.submitted = false;
+            main.name = "";
+            main.jobTitle = "";
+            main.tediousTask = "";
+            main.dirtyTask = "";
+            main.celebrity = "";
+            main.uselessSkill = "";
+            main.adjective = "";
+            main.obnoxiousCelebrity = "";
+            main.hugeNumber = "";
+            main.genderObject = "she";
+            main.genderPronoun = "her";
+            main.genderSpecial = "her";
+        };
+    });
